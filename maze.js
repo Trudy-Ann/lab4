5d900877b90c13904b6db6c940061278213d60ed
summerstarchica <trudyanncampbell.402@gmail.com>
2016-10-13 22:21:17 +0000

    excecise 2

--- maze.js
+++ maze.js
@@ -1,15 +1,36 @@
-window.onload= wallboundary;
-
+/*window.onload= wallboundary;
 
 
  function wallboundary(){
 
     // $("boundary1").style.backgroundColor = "#ff8888";
    var wall = document.getElementById("boundary1");
-  
-   
    
    wall.addEventListener("mouseover", function(){
        this.setAttribute("class", "boundary youlose");
+       
+       
+       document.querySelectorAll('.boundary');
+       
    });
+}*/
+window.onload = function() {
+    var wall1 = document.getElementById("boundary1");
+     wall1.addEventListener("mouseover", function(){
+     this.setAttribute("class", "boundary youlose");});
+     
+      var wall2 = document.querySelectorAll(".boundary");
+     
+    for (var i = 0; i < wall2.length; i++) {
+        wall2[i].onmouseover = overBoundary;
+    }
+
+
+
+function overBoundary() {
+    var boundary =  document.querySelectorAll (".boundary");
+    for (var i = 0; i < boundary.length; i++) {
+        boundary[i].style.backgroundColor = "#ff8888";
+    }
+}
 }
